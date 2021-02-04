8e9a82db2dffa8156a7baf01bef907e35ede7fba
Xanzis <xander@hughes-family.org>
2021-02-03 23:31:54 -0500

    refactor in progress

--- src/element/isopar.rs
+++ src/element/isopar.rs
@@ -3,20 +3,85 @@ use crate::spatial::Point;
 use std::convert::TryInto;
 use super::material::Material;
 use crate::matrix::inverse::Inverse;
+use super::strain::{StrainRule};
 
 // constructors for isoparametric finite element matrices
 
 pub trait IsoparElement {
-    fn dim(&self) -> usize;
-    fn ncount(&self) -> usize;
     fn new(nodes: Vec<Point>) -> Self;
-    fn nodes(&self) -> Vec<Point>;
+    fn dim(&self) -> usize;
+    fn node_count(&self) -> usize;
+    fn node(&self, i: usize) -> Point;
+    fn h_and_grad(&self, nat_coor: Point) -> Vec<(f64,  Point)>;
 
-    fn h_and_grad(&self, nat_coor: Point) -> Vec<(f64, (f64, f64))>;
-    fn find_mats(&self, nat_coor: Point) -> ElementMats;
+    fn nodes(&self) -> Vec<Point> {
+        (0..self.dim()).map(|x| self.node(x)).collect()
+    }
 
-    fn find_K_integrand(&self, nat_coor: Point, c: &LinearMatrix) -> LinearMatrix {
-        let mut mats = self.find_mats(nat_coor);
+    fn find_mats<T: StrainRule>(&self, nat_coor: Point, strain_rule: T) -> ElementMats {
+        // initialize dim (spatial dimension count) and n (node count)
+        let dim = self.dim();
+        let n = self.node_count();
+
+        let nodes = self.nodes();
+
+        // initialize matrices for construction
+        let mut j = LinearMatrix::zeros((dim, dim));
+        let mut nat_grad_interps = Vec::new();
+        for i in 0..dim {
+            nat_grad_interps.push(LinearMatrix::zeros((dim, dim * n)));
+        }
+        let mut h_mat = LinearMatrix::zeros((dim, dim * n));
+
+        let mut funcs = self.h_and_grad(nat_coor);
+        for (i, (h, nat_grad_h)) in funcs.into_iter().enumerate() {
+            let node_loc = self.node(i);
+            for x in 0..dim {
+                // h_mat is a simple arrangement of h values
+                h_mat[(x, (dim * i) + x)] = h;
+
+                // partial derivative of h in r/s/t (x is current dimension)
+                let nat_grad_in_dim = nat_grad_h[x];
+
+                for y in 0..dim {
+                    // J[x, y] is d (x, y, z)[y] / d (r, s, t)[x]
+                    // values interpolated from (d h_i / d (r/s/t)) * (x/y/z)_i
+                    j[(x, y)] += nat_grad_in_dim * node_loc[y];
+
+                    // fill out natural (r/s/t) gradient interpolations
+                    // by the end, ngi[y] * U = grad_rst (u, v, w)[y]
+                    nat_grad_interps[y][(x, (dim * i) + y)] = nat_grad_in_dim;
+                }
+            }
+        }
+
+        // invert J to find the gradient interpolation for the global coordinate system
+        let j_inv = j.inverse();
+        let det_j = j.determinant();
+
+        // construct the strain interpolation matrix
+        let mut b = LinearMatrix::zeros((strain_rule.vec_len(), dim * n));
+
+        for (i, nat_grad_interp) in nat_grad_interps.into_iter().enumerate() {
+            // ngi is the natural coordinate gradient interpolation for (u, v, w)[i]
+            let grad_interp = j_inv.mul(&nat_grad_interp);
+            // each row of gi is an interpolation for d(u/v/w)/dx, d(u/v/w)/dy ...
+            for j in 0..dim {
+                // if ngi[j, ..] should be added to a row in b, add it
+                if let Some(idx) = strain_rule.dest_idx(i, j) {
+                    for (k, coeff) in nat_grad_interp.row(j).cloned().enumerate() {
+                        b[(idx, k)] += coeff;
+                    }
+                }
+            }
+        }
+
+        // return it all :)
+        ElementMats {b, h: Some(h_mat), j, j_inv, det_j}
+    }
+
+    fn find_k_integrand<T: StrainRule>(&self, nat_coor: Point, c: &LinearMatrix, strain_rule: T) -> LinearMatrix {
+        let mut mats = self.find_mats(nat_coor, strain_rule);
 
         let mut inter = c.mul(&mats.b);
         inter *= mats.det_j;
@@ -33,42 +98,34 @@ pub struct ElementMats {
     b: LinearMatrix,
     h: Option<LinearMatrix>,
     j: LinearMatrix,
+    j_inv: LinearMatrix,
     det_j: f64,
 }
 
 impl IsoparElement for Bar2Node {
-    fn dim(&self) -> usize {
-        1
-    }
-
-    fn ncount(&self) -> usize {
-        2
-    }
-
     fn new(nodes: Vec<Point>) -> Bar2Node {
         if nodes.len() != 2 { panic!("bad node count") }
         Bar2Node { nodes: [nodes[0].try_into().unwrap(), nodes[1].try_into().unwrap()] }
     }
 
-    fn nodes(&self) -> Vec<Point> {
-        vec![self.nodes[0].into(), self.nodes[1].into()]
+    fn dim(&self) -> usize {
+        1
     }
 
-    fn h_and_grad(&self, nat_coor: Point) -> Vec<(f64, (f64, f64))> {
+    fn node_count(&self) -> usize {
+        2
+    }
+
+    fn node(&self, i: usize) -> Point {
+        self.nodes[i].into()
+    }
+
+    fn h_and_grad(&self, nat_coor: Point) -> Vec<(f64, Point)> {
         unimplemented!()
     }
 
-    fn find_mats(&self, nat_coor: Point) -> ElementMats {
+    fn find_mats<T: StrainRule>(&self, nat_coor: Point, strain_rule: T) -> ElementMats {
         unimplemented!()
-    }
-
-    fn find_K_integrand(&self, nat_coor: Point, c: &LinearMatrix) -> LinearMatrix {
-        let nat_coor: f64 = nat_coor.try_into().expect("dimension mismatch");
-        let l = self.nodes[1] - self.nodes[0];
-
-        let mut res = LinearMatrix::from_flat((2, 2), vec![1.0, -1.0, -1.0, 1.0]);
-        res *= l / 2.0;
-        res
     }
 }
 
@@ -77,14 +134,6 @@ pub struct PlaneNNode {
 }
 
 impl IsoparElement for PlaneNNode {
-    fn dim(&self) -> usize {
-        2
-    }
-
-    fn ncount(&self) -> usize {
-        self.nodes.len()
-    }
-
     fn new(nodes: Vec<Point>) -> Self {
         // initializes a new planar (1..=9)-node element
         // element should resemble:
@@ -101,76 +150,19 @@ impl IsoparElement for PlaneNNode {
         PlaneNNode {nodes}
     }
 
-    fn nodes(&self) -> Vec<Point> {
-        self.nodes.iter().cloned().map(|x| x.into()).collect()
+    fn dim(&self) -> usize {
+        2
     }
 
-    fn find_mats(&self, nat_coor: Point) -> ElementMats {
-        // assemble the various finite element matrices
-        // TODO: have option to only calculate desired matrices
-        let interps = self.h_and_grad(nat_coor);
-        let n = self.ncount();
-        let nodes = self.nodes();
-
-        let mut dxdr = 0.0;
-        let mut dydr = 0.0;
-        let mut dxds = 0.0;
-        let mut dyds = 0.0;
-        
-        // the following two matrices are interpolation matrices
-        // for grad u and grad v, respectively (in r, s)
-        let mut u_nat_grad_interp = LinearMatrix::zeros((2, 2 * n));
-        let mut v_nat_grad_interp = LinearMatrix::zeros((2, 2 * n));
-
-        // general position / displacement interpolation matrix
-        let mut h_mat = LinearMatrix::zeros((2, 2 * n));
-
-        // incorporate the gradient of each interpolation function 
-        for (i, (h, (dhdr, dhds))) in interps.into_iter().enumerate() {
-            let node = nodes[i];
-            let x = node[0];
-            let y = node[1];
-
-            // first row of j is dx/dr, dy/dr
-            dxdr += x * dhdr;
-            dydr += y * dhdr;
-            // second row is dx/ds, dy/ds
-            dxds += x * dhds;
-            dyds += y * dhds;
-
-            u_nat_grad_interp.put((0, 2 * i), dhdr);
-            u_nat_grad_interp.put((1, 2 * i), dhds);
-
-            v_nat_grad_interp.put((0, (2 * i) + 1), dhdr);
-            v_nat_grad_interp.put((1, (2 * i) + 1), dhds);
-
-            h_mat.put((0, 2 * i), h);
-            h_mat.put((0, (2 * i) + 1), h);
-        }
-
-        // construct J, invert it, get its determinant
-        let j = LinearMatrix::from_flat((2, 2), vec![dxdr, dydr, dxds, dyds]);
-        let det_j = j.determinant();
-        let j_inv = j.inverse();
-
-        // assemble x/y gradient interpolation matrices
-        let u_grad_interp = j_inv.mul(&u_nat_grad_interp);
-        let v_grad_interp = j_inv.mul(&v_nat_grad_interp);
-
-        // find strain interpolations (row vectors)
-        let e_xx: Vec<f64> = u_grad_interp.row(0).cloned().collect();
-        let e_yy: Vec<f64> = v_grad_interp.row(1).cloned().collect();
-        let e_xy: Vec<f64> = u_grad_interp.row(1)
-            .zip(v_grad_interp.row(0))
-            .map(|(x, y)| x + y)
-            .collect();
-
-        let b = LinearMatrix::from_rows(vec![e_xx, e_yy, e_xy]);
-
-        ElementMats { b, h: None, j, det_j }
+    fn node_count(&self) -> usize {
+        self.nodes.len()
     }
 
-    fn h_and_grad(&self, nat_coor: Point) -> Vec<(f64, (f64, f64))> {
+    fn node(&self, i: usize) -> Point {
+        self.nodes[i].into()
+    }
+
+    fn h_and_grad(&self, nat_coor: Point) -> Vec<(f64, Point)> {
         // calculate interpolation functions and their derivatives
         // interpolation is x = sum( h_i(r, s)*x_i )
         // returns (h_i, (dh_i/dr, dh_i/ds)) for i in 0..n
@@ -197,7 +189,10 @@ impl IsoparElement for PlaneNNode {
         dhds[3] = -0.25 * (1.0 + r);
 
         if self.dim() <= 4 {
-            return h.into_iter().zip(dhdr.into_iter().zip(dhds.into_iter())).collect()
+            return h.into_iter()
+                .zip(dhdr.into_iter().zip(dhds.into_iter()))
+                .map(|(x, y)| (x, y.into()))
+                .collect()
         }
 
         let temp_h = 0.5 * (1.0 - (r * r)) * (1.0 + s);
@@ -214,7 +209,10 @@ impl IsoparElement for PlaneNNode {
         dhds[1] -= 0.5 * temp_h;
 
         if self.dim() <= 5 {
-            return h.into_iter().zip(dhdr.into_iter().zip(dhds.into_iter())).collect()
+            return h.into_iter()
+                .zip(dhdr.into_iter().zip(dhds.into_iter()))
+                .map(|(x, y)| (x, y.into()))
+                .collect()
         }
 
         let temp_h = 0.5 * (1.0 - (s * s)) * (1.0 - r);
@@ -231,7 +229,10 @@ impl IsoparElement for PlaneNNode {
         dhds[2] -= 0.5 * temp_h;
 
         if self.dim() <= 6 {
-            return h.into_iter().zip(dhdr.into_iter().zip(dhds.into_iter())).collect()
+            return h.into_iter()
+                .zip(dhdr.into_iter().zip(dhds.into_iter()))
+                .map(|(x, y)| (x, y.into()))
+                .collect()
         }
 
         let temp_h = 0.5 * (1.0 - (r * r)) * (1.0 - s);
@@ -248,7 +249,10 @@ impl IsoparElement for PlaneNNode {
         dhds[3] -= 0.5 * temp_h;
 
         if self.dim() <= 7 {
-            return h.into_iter().zip(dhdr.into_iter().zip(dhds.into_iter())).collect()
+            return h.into_iter()
+                .zip(dhdr.into_iter().zip(dhds.into_iter()))
+                .map(|(x, y)| (x, y.into()))
+                .collect()
         }
 
         let temp_h = 0.5 * (1.0 - (s * s)) * (1.0 + r);
@@ -265,7 +269,10 @@ impl IsoparElement for PlaneNNode {
         dhds[3] -= 0.5 * temp_h;
 
         if self.dim() <= 7 {
-            return h.into_iter().zip(dhdr.into_iter().zip(dhds.into_iter())).collect()
+            return h.into_iter()
+                .zip(dhdr.into_iter().zip(dhds.into_iter()))
+                .map(|(x, y)| (x, y.into()))
+                .collect()
         }
 
         let temp_h = (1.0 - (r * r)) * (1.0 - (s * s));
@@ -285,6 +292,9 @@ impl IsoparElement for PlaneNNode {
             dhds[i] -= 0.5 * temp_dhds;
         }
 
-        h.into_iter().zip(dhdr.into_iter().zip(dhds.into_iter())).collect()
+        return h.into_iter()
+                .zip(dhdr.into_iter().zip(dhds.into_iter()))
+                .map(|(x, y)| (x, y.into()))
+                .collect()
     }
 }
--- src/element/mod.rs
+++ src/element/mod.rs
@@ -1,5 +1,6 @@
 pub mod material;
 pub mod isopar;
+pub mod strain;
 
 #[cfg(test)]
 mod tests;
