d6e3566769c89fc04ad4eb443b984381b007f051
Xanzis <xander@xanderh.me>
2021-04-27 00:04:28 -0400

    meshing procedure and tests

--- src/mesher/mod.rs
+++ src/mesher/mod.rs
@@ -1,1 +1,59 @@
+use std::error;
+use std::fmt;
+
+pub mod bounds;
 pub mod plane;
+
+#[derive(Clone, Debug)]
+pub enum MeshError {
+    TriangleError(String),
+}
+
+impl MeshError {
+    fn triangle<T: ToString>(msg: T) -> Self {
+        MeshError::TriangleError(msg.to_string())
+    }
+}
+
+impl fmt::Display for MeshError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            MeshError::TriangleError(s) => write!(f, "TriangleError: {}", s),
+        }
+    }
+}
+
+impl error::Error for MeshError {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn chew_bound() {
+        use super::bounds;
+
+        let mut b = bounds::PlaneBoundary::new();
+        b.store_polygon(vec![(0.0, 0.0), (2.0, 0.3), (2.0, 5.0), (-1.0, 3.5)]);
+
+        b.divide_all_segments(1.0);
+
+        let mut vis = b.visualize();
+
+        vis.draw("test_generated/chew_bound.png", ());
+    }
+
+    #[test]
+    fn chew_mesh() {
+        use super::bounds;
+        use super::plane;
+
+        let mut b = bounds::PlaneBoundary::new();
+        b.store_polygon(vec![(0.0, 0.0), (2.0, 0.3), (2.0, 5.0), (-1.0, 3.5)]);
+        b.divide_all_segments(1.0);
+
+        let mut mesh = plane::PlaneTriangulation::new(b);
+        mesh.gift_wrap();
+
+        let mut vis = mesh.visualize();
+        vis.draw("test_generated/chew_mesh.png", ());
+    }
+}
--- src/mesher/plane.rs
+++ src/mesher/plane.rs
@@ -1,30 +1,39 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::convert::{From, Into, TryInto};
 use std::iter;
 
-use rand::seq::SliceRandom;
-
 use crate::spatial::predicates::{self, Orient};
 use crate::spatial::Point;
+use crate::visual::Visualizer;
+
+use super::bounds::{self, PlaneBoundary, Segment};
+use super::MeshError;
 
 // enum for vertex indices in the ghost vertex scheme
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 enum VIdx {
     Real(usize),
+    Bound(bounds::VIdx),
     Ghost,
 }
 
 impl VIdx {
     fn is_ghost(&self) -> bool {
         match self {
-            VIdx::Real(_) => false,
             VIdx::Ghost => true,
+            _ => false,
         }
     }
     fn is_real(&self) -> bool {
         match self {
             VIdx::Real(_) => true,
-            VIdx::Ghost => false,
+            _ => false,
+        }
+    }
+    fn is_bound(&self) -> bool {
+        match self {
+            VIdx::Bound(_) => true,
+            _ => false,
         }
     }
 }
@@ -85,7 +94,16 @@ impl From<(VIdx, VIdx)> for Edge {
     }
 }
 
-struct PlaneTriangulation {
+impl From<Segment> for Edge {
+    fn from(s: Segment) -> Edge {
+        Edge(VIdx::Bound(s.0), VIdx::Bound(s.1))
+    }
+}
+
+pub struct PlaneTriangulation {
+    // mesh bounds structure
+    bound: PlaneBoundary,
+
     // vector of vertex locations
     vertices: Vec<(f64, f64)>,
 
@@ -102,6 +120,18 @@ struct PlaneTriangulation {
 }
 
 impl PlaneTriangulation {
+    pub fn new(bound: PlaneBoundary) -> Self {
+        // initialize a new empty triangulation with the given boundary
+        PlaneTriangulation {
+            bound,
+            vertices: Vec::new(),
+            tris: HashMap::new(),
+            tri_idxs: HashMap::new(),
+            tri_list: Vec::new(),
+            recents: HashMap::new(),
+        }
+    }
+
     fn store_vertex<T: TryInto<(f64, f64)>>(&mut self, p: T) -> VIdx {
         // store a new vertex, returning its index
         if let Ok((x, y)) = p.try_into() {
@@ -120,16 +150,16 @@ impl PlaneTriangulation {
         // retrieve a vertex
         match v {
             VIdx::Real(i) => self.vertices.get(i).cloned(),
+            VIdx::Bound(i) => self.bound.get(i),
             VIdx::Ghost => None,
         }
     }
 
     fn all_vidx(&self) -> impl Iterator<Item = VIdx> {
         // return an iterator over all valid VIdxs
-        // TODO when applicable include constraint geometry's vertices
-        // (VIdx will end up with a Constraint variant with indices)
         (0..self.vertices.len())
             .map(|x| VIdx::Real(x))
+            .chain(self.bound.all_vidx().map(|x| VIdx::Bound(x)))
             .chain(iter::once(VIdx::Ghost))
     }
 
@@ -149,6 +179,12 @@ impl PlaneTriangulation {
     fn in_circle<T: Into<Triangle>>(&self, tri: T, x: VIdx) -> bool {
         // determine whether x lies in the oriented triangle tri's circumcircle
         let tri = tri.into();
+
+        // end immediately for doubly-ghost edge cases
+        if tri.ghost_count() > 1 {
+            return false
+        }
+
         // first, if tri is a ghost triangle, only check if x is to the left of the non-ghost segment
         let ghosts = (tri.0.is_ghost(), tri.1.is_ghost(), tri.2.is_ghost());
         if ghosts.0 {
@@ -161,12 +197,19 @@ impl PlaneTriangulation {
             return self.triangle_dir((tri.0, tri.1, x)).unwrap() != Orient::Negative;
         }
 
+        // a ghost vertex is never inside a real triangle
+        if x.is_ghost() {
+            return false
+        }
+
         // if tri is a proper triangle, use the in_circle spatial predicate
         // sanity check: tri should be correctly oriented (TODO remove this check)
         assert_eq!(self.triangle_dir(tri), Some(Orient::Positive));
 
+        println!("checking in_circle for {:?}, {:?}", self.get(x).unwrap(), self.get_triangle_points(tri).unwrap());
+
         predicates::in_circle(
-            self.get(x).unwrap().into(),
+            self.get(x).expect("nonexistent vidx").into(),
             self.get_triangle_points(tri).unwrap(),
         )
     }
@@ -189,16 +232,16 @@ impl PlaneTriangulation {
         }
     }
 
-    fn add_triangle<T: Into<Triangle>>(&mut self, tri: T) -> bool {
-        // add a triangle to the triangulation, returning true if successful
+    fn add_triangle<T: Into<Triangle>>(&mut self, tri: T) -> Result<(), MeshError> {
+        // add a triangle to the triangulation
         // only one of the vertices may be a ghost vertex
         // if no vertices are ghosts, as a sanity check ensure the triangle is positive
         let tri = tri.into();
         match tri.ghost_count() {
-            2 | 3 => return false,
+            2 | 3 => return Err(MeshError::triangle("doubly / triply ghost triangle")),
             0 => {
                 if self.triangle_dir(tri) != Some(Orient::Positive) {
-                    return false;
+                    return Err(MeshError::triangle("negative triangle addition requested"));
                 }
             }
             1 => (),
@@ -207,8 +250,8 @@ impl PlaneTriangulation {
 
         // a new triangle may not share an (oriented) edge with an old triangle
         let edges = tri.edges();
-        if edges.into_iter().any(|e| self.tris.contains_key(e)) {
-            return false;
+        if edges.iter().any(|e| self.tris.contains_key(e)) {
+            return Err(MeshError::triangle("existing triangle conflicts with edge"));
         }
 
         // triangle is ok to add - add it
@@ -217,31 +260,31 @@ impl PlaneTriangulation {
         self.tris.insert(edges[2], tri.1);
 
         let idx = self.tri_list.len();
-        for e in edges.into_iter().cloned() {
+        for e in edges.iter().cloned() {
             self.tri_idxs.insert(e, idx);
         }
         self.tri_list.push(tri);
 
         // add to the recent co-appearing points map for later adjacent_one use
         // seems reasonable to not insert any ghost pairs for adjacent_one, right?
-        for e in edges.into_iter().cloned() {
+        for e in edges.iter().cloned() {
             if !e.is_ghost() {
                 self.recents.insert(e.0, e.1);
                 self.recents.insert(e.1, e.0);
             }
         }
 
-        true
+        Ok(())
     }
 
-    fn delete_triangle<T: Into<Triangle>>(&mut self, tri: T) -> bool {
-        // delete a triangle, returning true if successful
+    fn delete_triangle<T: Into<Triangle>>(&mut self, tri: T) -> Result<(), MeshError> {
+        // delete a triangle
         let tri = tri.into();
         let edges = tri.edges();
         if self.tris.get(&edges[0]) == Some(&tri.2) {
             // store the idnex of the removed triangle and remove entries
             let idx = self.tri_idxs.get(&edges[0]).unwrap().clone();
-            for e in edges.into_iter() {
+            for e in edges.iter() {
                 self.tris.remove(e);
                 self.tri_idxs.remove(e);
             }
@@ -252,12 +295,12 @@ impl PlaneTriangulation {
             self.tri_list[idx] = to_move;
 
             // update the index of the moved triangle
-            for e in to_move.edges().into_iter().cloned() {
+            for e in to_move.edges().iter().cloned() {
                 self.tri_idxs.insert(e, idx);
             }
-            true
+            Ok(())
         } else {
-            false
+            Err(MeshError::triangle("triangle to be deleted does not exist"))
         }
     }
 
@@ -312,7 +355,8 @@ impl PlaneTriangulation {
                 self.bowyer_watson_dig(u, v, x);
                 self.bowyer_watson_dig(u, x, w);
             } else {
-                self.add_triangle((u, v, w));
+                self.add_triangle((u, v, w))
+                    .expect("could not add triangle");
             }
         }
     }
@@ -321,7 +365,8 @@ impl PlaneTriangulation {
         // insert a vertex into a delaunay triangulation, maintaining the delaunay property
         // tri is a triangle whose cirmcumcircle encloses u
         let Triangle(v, w, x) = tri.into();
-        self.delete_triangle((v, w, x));
+        self.delete_triangle((v, w, x))
+            .expect("could not delete triangle");
         self.bowyer_watson_dig(u, v, w);
         self.bowyer_watson_dig(u, w, x);
         self.bowyer_watson_dig(u, x, v);
@@ -341,10 +386,81 @@ impl PlaneTriangulation {
             }
             // proceed if v is in front of e and tri is either None or encircling
             if self.to_left(e, v) && tri.map_or(true, |t| self.in_circle(t, v)) {
+                // TODO add constrained visibility check here
                 tri = Some((e.0, e.1, v).into());
             }
         }
 
-        None
+        // return the triangle and the two new directed edges
+        tri.map(|t| (t, (t.1, t.2).into(), (t.2, t.0).into()))
+    }
+
+    pub fn gift_wrap(&mut self) {
+        // gift-wrap the triangulation from scratch
+        self.tris.clear();
+        self.tri_idxs.clear();
+        self.tri_list.clear();
+        self.recents.clear();
+
+        let mut to_finish: HashSet<Edge> = HashSet::new();
+
+        for e in self.bound.all_walls().into_iter().map(|s| s.into()) {
+            to_finish.insert(e);
+        }
+
+        while !to_finish.is_empty() {
+            // remove an element from the set
+            let e = to_finish.iter().next().cloned().unwrap();
+            to_finish.remove(&e);
+
+            if let Some((tri, a, b)) = self.gift_wrap_finish(e) {
+                self.add_triangle(tri).expect("could not add triangle");
+                if !to_finish.remove(&a) {
+                    // if a was not in set, add its reverse
+                    to_finish.insert(a.rev());
+                }
+                if !to_finish.remove(&b) {
+                    to_finish.insert(b.rev());
+                }
+            }
+        }
+
+        let mut vis = self.visualize();
+        vis.draw(format!("test_generated/eek.png").as_str(), ());
+    }
+
+    pub fn visualize(&self) -> Visualizer {
+        let vidxs: Vec<VIdx> = self.all_vidx().filter(|x| !x.is_ghost()).collect();
+
+        let nodes: Vec<Point> = vidxs.iter().map(|x| self.get(*x).unwrap().into()).collect();
+
+        let idx_map: HashMap<VIdx, usize> = vidxs
+            .iter()
+            .cloned()
+            .enumerate()
+            .map(|(x, y)| (y, x))
+            .collect();
+
+        let mut edge_set: HashSet<Edge> = HashSet::new();
+        for e in self.tris.keys().cloned() {
+            if !edge_set.contains(&e.rev()) {
+                edge_set.insert(e);
+            }
+        }
+
+        let edges: Vec<(usize, usize)> = edge_set
+            .drain()
+            .filter(|e| !e.is_ghost())
+            .map(|e| {
+                (
+                    idx_map.get(&e.0).cloned().unwrap(),
+                    idx_map.get(&e.1).cloned().unwrap(),
+                )
+            })
+            .collect();
+
+        let mut vis: Visualizer = nodes.into();
+        vis.set_edges(edges);
+        vis
     }
 }
